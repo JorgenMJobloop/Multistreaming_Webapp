1e8b6197c26c1b3dabe0c9f15bda2511f32f3dec
JorgenMJobloop <jorgen@jobloop.no>
2024-02-16 10:45:07 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -15,18 +15,23 @@ function closeSidepanel() {
     document.getElementById("open-sidepanel-btn").style.display = "block";
     getContent.style.display = "block";
     document.getElementById("media-content").style.display = "none";
-
+    ifSidePanelClosed();
 }
 
 
-
-function whenPlaying() {
-    return countContent.textContent += 1;
-}
 
 
 document.addEventListener("DOMContentLoaded", () => {
     const videoPlayer = document.getElementById("video-player");
+    const videoTitleElement = document.getElementById("video-title");
+    const videoDescriptionElement = document.getElementById("video-description");
+    const videoFeedURLArray = ["./src/video/async.mp4", "./src/video/hashtable.mp4"];
+    const videoMetadata = {
+        title: ``,
+        description: ``,
+        thumbnail: "./src/thumbnails/Thumb1",
+    };
+    const videoThumbnails = document.createElement("img");
 
     async function fetchVideoFeed(url) {
         try {
@@ -35,13 +40,28 @@ document.addEventListener("DOMContentLoaded", () => {
             const videoURL = URL.createObjectURL(videoBlob);
 
             videoPlayer.src = videoURL;
+            videoPlayer.volume = 0.3;
             videoPlayer.play();
+
+            /* Write some code that display more information about
+               the video-metadata in markdown
+            */
+
+            videoTitleElement.textContent = videoMetadata.title;
+            videoDescriptionElement.textContent = videoMetadata.description;
+            videoThumbnails.src = `${videoMetadata.thumbnail}`
+
+            window.ifSidePanelClosed = () => {
+                videoPlayer.pause();
+                closeSidepanel();
+            }
         }
         catch (error) {
             console.error(`Error fetching video feed: ${error}`);
         }
     }
     // driver code with arguments!
-    const videoFeedURL = "";
+    const videoFeedURL = videoFeedURLArray[0];
     fetchVideoFeed(videoFeedURL);
+
 })
